170406d08f2d1ceb5d42c2a90457b158c92b51e2
Dan Friedman <dan.friedma@gmail.com>
2014-02-10 23:23:01 -0500

    root => home#emptyroute and /home => home#index

--- config/routes.rb
+++ config/routes.rb
@@ -5,6 +5,10 @@ Jardin::Application.routes.draw do
   # You can have the root of your site routed with "root"
   # root 'welcome#index'
 
+root :to => 'home#emptyroute'
+
+get '/home' => 'home#index'
+
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
 
