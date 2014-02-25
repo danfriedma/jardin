822fcadb23105f3c1cbc9c493fbb7a64f6fcf18f
Dan Friedman <dan.friedma@gmail.com>
2014-02-25 00:00:13 -0500

    updated readme

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,6 +3,7 @@ class User
   #include Mongoid::Ancestry
   #has_ancestry
   has_many :comments, :as => :commentable
+  belongs_to :article
   field :name, type: String
   field :password, type: String
   field :email, type: String
